9383c17e489423ac45a0dd32ba5a4d6933da7fb0
nguyenthinghia <nguyennghia732k5@gmail.com>
2026-07-06 22:27:12 +0700

    Initial Commit

---
+++ DV_XE_MANH_HUNG/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using DV_XE_MANH_HUNG.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DV_XE_MANH_HUNG.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DV_XE_MANH_HUNG/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DV_XE_MANH_HUNG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
